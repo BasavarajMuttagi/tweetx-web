27e0e067b7ee689deb4e52e4175fc7cad78b2b0f
BasavarajMuttagi <basavaraj2770@gmail.com>
2024-01-17 16:39:56 +0530

    isFetched added

--- src/components/Users.component.tsx
+++ src/components/Users.component.tsx
@@ -4,25 +4,27 @@ import { Fragment } from "react";
 import { getAllUsers } from "../Endpoints/InternalEndpoints";
 
 function Users() {
-  const { isLoading, error, data } = useQuery({
+  const { isLoading, error, data, isFetched } = useQuery({
     queryKey: ["getallusers"],
     queryFn: async () => getAllUsers(),
+    initialData: [],
   });
 
   if (isLoading) return <div className="font-bold text-2xl">Loading....</div>;
   if (error) return "An error has occurred: " + error.message;
   return (
     <div className="space-y-2 w-full max-w-screen-md ">
-      {data?.users?.map((eachUser: any) => (
-        <Fragment key={eachUser._id}>
-          <UserCard
-            name={eachUser.name}
-            email={eachUser.email}
-            _id={eachUser._id}
-          />
-          <div className="broder-t bg-black border"></div>
-        </Fragment>
-      ))}
+      {isFetched &&
+        data?.users?.map((eachUser: any) => (
+          <Fragment key={eachUser._id}>
+            <UserCard
+              name={eachUser.name}
+              email={eachUser.email}
+              _id={eachUser._id}
+            />
+            <div className="broder-t bg-black border"></div>
+          </Fragment>
+        ))}
     </div>
   );
 }
